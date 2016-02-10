fe54390da5b1111c3f1f216e4bbb9660d90550c4
sergeyt <stodyshev@gmail.com>
2016-02-10 19:48:33 +0600

    lint code

--- src/hover.js
+++ src/hover.js
@@ -1,7 +1,7 @@
 import classNames from 'classnames';
 import style from 'hover.css/scss/hover';
 
-function identity(t) { return t; };
+function identity(t) { return t; }
 
 export default function hover(...effects) {
 	const classList = effects.filter(identity).map(name => style[`hvr-${name}`]);
